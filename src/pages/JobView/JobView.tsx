26c229baacb45653d4faf160d47780aaaeb691b2
Jessica O'Brien <107439850+jessica-obrien@users.noreply.github.com>
2022-07-09 00:06:24 +0000

    #73 - cutting unecessary grids

--- src/pages/JobView/JobView.tsx
+++ src/pages/JobView/JobView.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import { AxiosResponse } from 'axios'
 import PageHeader from 'components/PageHeader'
 import { JobButton } from 'pages/JobView'
@@ -30,26 +30,20 @@ const JobView = () => {
 
   return (
     <Box>
-      <Grid justifyContent="space-between" container>
-        <Grid item>
-          <PageHeader title="Job" description={description} />
-        </Grid>
-        <Grid item>
-          <Typography style={{ flex: 1 }}>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={deleteButtonCallback}
-            >
-              Delete Job
-            </Button>
-            <JobButton job={job as Job} id={id} callback={jobButtonCallback} />
-            <Button variant="contained" color="primary">
-              Update Job
-            </Button>
-          </Typography>
-        </Grid>
-      </Grid>
+      <Typography style={{ flex: 1, float: 'right' }}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={deleteButtonCallback}
+        >
+          Delete Job
+        </Button>
+        <JobButton job={job as Job} id={id} callback={jobButtonCallback} />
+        <Button variant="contained" color="primary">
+          Update Job
+        </Button>
+      </Typography>
+      <PageHeader title="Job" description={description} />
     </Box>
   )
 }
