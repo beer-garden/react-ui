cf73bb536ff38382cbad6d17599583892ef24283
bjwest3 <bjwest31@outlook.com>
2022-07-18 11:55:47 +0000

    changed datetime picker to native picker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { LocalizationProvider } from '@mui/lab'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { Routes } from 'components/Routes'
 import Layout from 'components/UI/Layout'
+import moment from 'moment-timezone'
+
+moment.tz.setDefault('UTC')
 
 const App = (): JSX.Element => {
   return (
