a1e063dc034a4de970fd27683c7b02e00c0d60fe
Jessica O'Brien <107439850+jessica-obrien@users.noreply.github.com>
2022-08-18 20:21:34 +0000

    #89 - add queue hook

---
+++ src/hooks/useQueue.tsx
@@ -0,0 +1,45 @@
+import { ServerConfigContainer } from 'containers/ConfigContainer'
+import { useMyAxios } from 'hooks/useMyAxios'
+import { Queue } from 'types/backend-types'
+import { System } from 'types/custom_types'
+
+const useQueue = () => {
+  const { authEnabled } = ServerConfigContainer.useContainer()
+  const { axiosInstance } = useMyAxios()
+
+  const getInstanceQueues = (instanceId: string) => {
+    return axiosInstance.get<Queue[]>(
+      `/api/v1/instances/${instanceId}/queues`,
+      {
+        withCredentials: authEnabled,
+      },
+    )
+  }
+
+  const clearQueue = (name: string) => {
+    return axiosInstance.delete<System[]>(`/api/v1/queues/${name}`, {
+      withCredentials: authEnabled,
+    })
+  }
+
+  const clearQueues = () => {
+    return axiosInstance.delete<System[]>('/api/v1/queues/', {
+      withCredentials: authEnabled,
+    })
+  }
+
+  const getQueues = () => {
+    return axiosInstance.get<System[]>('/api/v1/queues/', {
+      withCredentials: authEnabled,
+    })
+  }
+
+  return {
+    getInstanceQueues,
+    clearQueues,
+    clearQueue,
+    getQueues,
+  }
+}
+
+export default useQueue
