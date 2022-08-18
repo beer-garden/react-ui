dcb58429cec9cf19d0f125b71facdf7c7a04e69e
Jessica O'Brien <107439850+jessica-obrien@users.noreply.github.com>
2022-08-18 20:25:10 +0000

    #89 - implement Clear All Queues

--- src/pages/SystemAdmin/SystemAdmin.tsx
+++ src/pages/SystemAdmin/SystemAdmin.tsx
@@ -1,11 +1,14 @@
+import { Typography } from '@mui/material'
 import { Alert, Box, Button, Divider, Grid, Tooltip } from '@mui/material'
+import { ModalWrapper } from 'components/ModalWrapper'
 import PageHeader from 'components/PageHeader'
 import useAdmin from 'hooks/useAdmin'
 import { useLocalStorage } from 'hooks/useLocalStorage'
 import useNamespace from 'hooks/useNamespace'
+import useQueue from 'hooks/useQueue'
 import { NamespaceCard } from 'pages/SystemAdmin/NamespaceCard'
 import { NamespaceSelect } from 'pages/SystemAdmin/NamespaceSelect'
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 const getSelectMessage = (namespacesSelected: string[]): JSX.Element | void => {
   if (!namespacesSelected.length) {
@@ -33,6 +36,7 @@ const SystemAdmin = () => {
     'namespacesSelected',
     [],
   )
+  const [open, setOpen] = useState(false)
 
   const contextValue = {
     namespaces: useNamespace(),
@@ -41,6 +45,7 @@ const SystemAdmin = () => {
   }
 
   const adminClient = useAdmin()
+  const queueClient = useQueue()
 
   return (
     <Box>
@@ -67,13 +72,18 @@ const SystemAdmin = () => {
           flexDirection="row"
         >
           <Tooltip arrow title="Clear All Queues">
-            <Button variant="contained" color="secondary" sx={{ m: 1 }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{ m: 1 }}
+              onClick={() => setOpen(true)}
+            >
               Clear
             </Button>
           </Tooltip>
           <Tooltip arrow title="Rescan Plugin Directory">
             <Button
-              onClick={() => adminClient.rescanPluginDirectory()}
+              onClick={adminClient.rescanPluginDirectory}
               variant="contained"
               color="primary"
               sx={{ m: 1 }}
@@ -83,6 +93,23 @@ const SystemAdmin = () => {
           </Tooltip>
         </Grid>
       </Grid>
+      <ModalWrapper
+        open={open}
+        header="Clear All Queues?"
+        onClose={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        onSubmit={() => {
+          queueClient.clearQueues()
+          setOpen(false)
+        }}
+        content={
+          <Typography my={2}>
+            All outstanding requests for the system will be deleted. This cannot
+            be undone.
+          </Typography>
+        }
+        styleOverrides={{ width: '35%' }}
+      />
       <Divider sx={{ my: 2 }} />
       {namespacesSelected.map((namespace: string) => (
         <NamespaceCard namespace={namespace} key={namespace + 'card'} />
---
+++ src/types/backend-types.ts
@@ -0,0 +1,9 @@
+export interface Queue {
+  version: string
+  system: string
+  size: number
+  instance: string
+  system_id: string
+  display: string
+  name: string
+}
