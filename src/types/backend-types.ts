aafc858c88a2e184219a530043bb6249e59896c1
John B <jlrpnbbngtn@gmail.com>
2022-08-18 09:44:15 -0400

    Cleaned up 'any's in backend types

--- src/types/backend-types.ts
+++ src/types/backend-types.ts
@@ -34,7 +34,7 @@ export type ParameterType =
   | 'Any'
   | 'DateTime'
   | 'Float'
-  | 'ObjectWithStringKeys'
+  | 'Dictionary'
 
 export type Parameter = {
   key: string
@@ -51,7 +51,7 @@ export type Parameter = {
   minimum?: number
   regex?: string
   form_input_type?: 'textarea'
-  type_info: any
+  type_info?: ObjectWithStringKeys | null
 }
 
 export type Instance = {
@@ -59,34 +59,39 @@ export type Instance = {
   description: string
   id: string
   status: string
-  status_info: any
+  status_info: StatusInfo
   queue_type: string
-  queue_info: any
-  icon_name: string
-  metadata: any
+  queue_info?: ObjectWithStringKeys | null
+  icon_name?: string
+  metadata?: ObjectWithStringKeys | null
+}
+
+export interface StatusInfo {
+  heartbeat: number
 }
 export interface Request {
   children: Request[]
   command: string
   command_type: string
   comment: string | null
-  created_at: number
+  created_at?: number
   error_class: Error | null
-  has_parent: boolean
-  hidden: boolean
-  id: string
+  has_parent?: boolean
+  hidden?: boolean
+  id?: string
   instance_name: string
-  metadata: any
+  metadata?: ObjectWithStringKeys | EmptyObject
   namespace: string
-  output: string
+  output?: string
   output_type: string
   parameters: Parameter[]
   parent: Request | null
-  requester: string
+  requester?: string
   status: string
+  status_updated_at?: number
   system: string
   system_version: string
-  updated_at: number
+  updated_at?: number
 }
 
 export interface RequestTemplate {
@@ -113,7 +118,7 @@ export interface System {
   commands: Command[]
   icon_name: string
   display_name: string
-  metadata: any
+  metadata?: ObjectWithStringKeys | EmptyObject
   local: boolean
   template: string
 }
@@ -185,9 +190,9 @@ export interface Garden {
   id: string
   name: string
   status: string
-  status_info: any
+  status_info: StatusInfo
   namespaces: string[]
   systems: System[]
   connection_type: string
-  connection_params: any
+  connection_params?: ObjectWithStringKeys | EmptyObject | null
 }
