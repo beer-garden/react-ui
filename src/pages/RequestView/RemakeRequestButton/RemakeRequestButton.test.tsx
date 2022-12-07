90cbe14043cd1f7a3a80abcc4acd3ce7a76456be
John B <jlrpnbbngtn@gmail.com>
2022-12-07 14:55:57 -0500

    More unit tests

--- src/pages/RequestView/RemakeRequestButton/RemakeRequestButton.test.tsx
+++ src/pages/RequestView/RemakeRequestButton/RemakeRequestButton.test.tsx
@@ -1,12 +1,15 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import {
   emptyJobRequestCreationProviderState,
   JobRequestCreationContext,
 } from 'components/JobRequestCreation'
+import { Dispatch, SetStateAction } from 'react'
 import { AllProviders } from 'test/testMocks'
+import { AugmentedCommand, StrippedSystem } from 'types/custom-types'
+import { CommandViewRequestModel } from 'types/form-model-types'
 
 import { RemakeRequestButton } from './RemakeRequestButton'
-import { buttonText, DummyRequest } from './remakeRequestButtonHelpers'
+import { DummyRequest } from './remakeRequestButtonHelpers'
 
 describe('RemakeRequestButton', () => {
   afterAll(() => {
@@ -23,6 +26,40 @@ describe('RemakeRequestButton', () => {
           value={emptyJobRequestCreationProviderState}
         >
           <RemakeRequestButton request={DummyRequest} />
+        </JobRequestCreationContext.Provider>
+      </AllProviders>,
+    )
+    fireEvent.mouseOver(screen.getByTestId('cannot-execute-button'))
+    expect(await screen.findByText(errorText)).toBeInTheDocument()
+  })
+
+  test('Cannot execute on undefined request', async () => {
+    const errorText = 'ERROR: Request not available'
+    const dummySetter = () => undefined
+
+    render(
+      <AllProviders>
+        <JobRequestCreationContext.Provider
+          value={{
+            ...emptyJobRequestCreationProviderState,
+            setSystem: dummySetter as unknown as Dispatch<
+              SetStateAction<StrippedSystem | undefined>
+            >,
+            setCommand: dummySetter as unknown as Dispatch<
+              SetStateAction<AugmentedCommand | undefined>
+            >,
+            setIsJob: dummySetter as unknown as Dispatch<
+              SetStateAction<boolean>
+            >,
+            setRequestModel: dummySetter as unknown as Dispatch<
+              SetStateAction<CommandViewRequestModel | undefined>
+            >,
+            setIsReplay: dummySetter as unknown as Dispatch<
+              SetStateAction<boolean>
+            >,
+          }}
+        >
+          <RemakeRequestButton request={undefined} />
         </JobRequestCreationContext.Provider>
       </AllProviders>,
     )
