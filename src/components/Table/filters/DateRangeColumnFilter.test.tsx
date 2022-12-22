dfb8e7af004b4eb9ab84b06d5f132c93d7d292e0
John B <jlrpnbbngtn@gmail.com>
2022-12-22 07:35:44 -0500

    #292 - Fixed date-related unit tests

--- src/components/Table/filters/DateRangeColumnFilter.test.tsx
+++ src/components/Table/filters/DateRangeColumnFilter.test.tsx
@@ -5,13 +5,36 @@ import { ObjectWithStringKeys } from 'types/custom-types'
 
 import { DateRangeColumnFilter } from './DateRangeColumnFilter'
 
+const dateTranslator = (dateString: string) => {
+  // this abomination is the best I could come up with without bringing
+  // in a library, which would be pointless
+  return (
+    new Intl.DateTimeFormat('en-CA', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    }).format(new Date(dateString)) +
+    'T' +
+    new Intl.DateTimeFormat('en-US', {
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    }).format(new Date(dateString))
+  )
+}
+
 describe('DateRangeColumnFilter', () => {
   test('renders cell string value', () => {
+    const [from, to] = TDateFilter.filterValue.map(dateTranslator)
     const testProps = {
       column: TDateFilter,
     } as FilterProps<ObjectWithStringKeys>
+
     render(<DateRangeColumnFilter {...testProps} />)
-    expect(screen.getByDisplayValue('2022-10-14T16:01')).toBeInTheDocument()
-    expect(screen.getByDisplayValue('2022-11-24T16:02')).toBeInTheDocument()
+
+    expect(screen.getByDisplayValue(from)).toBeInTheDocument()
+    expect(screen.getByDisplayValue(to)).toBeInTheDocument()
   })
 })
