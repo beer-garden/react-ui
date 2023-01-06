70dfb8ea585cec246b7fcd6b98db787c2ddbda14
obr42 <107439850+obr42@users.noreply.github.com>
2023-01-06 19:25:08 +0000

    #220 - give request comment more space

--- src/pages/RequestView/RequestViewTable.tsx
+++ src/pages/RequestView/RequestViewTable.tsx
@@ -120,7 +120,9 @@ const RequestViewTable = ({ request }: RequestViewTableProps) => {
           data={dateFormatted(new Date(request.updated_at))}
         />
         {request.comment && (
-          <LabeledData label="Comment" data={request.comment || ''} />
+          <Box sx={{ gridColumnStart: '1', gridColumnEnd: -1 }}>
+            <LabeledData label="Comment" data={request.comment || ''} />
+          </Box>
         )}
       </Box>
       {request.children[0] ? (
