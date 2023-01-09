8e0d7918f6c29c5711a4dce87aaa202e3c58bb1e
obr42 <107439850+obr42@users.noreply.github.com>
2023-01-09 12:37:22 -0500

    #306 - Tweak disabled buttons in dark mode (#330)

--- src/pages/RequestView/RemakeRequestButton/CannotReExecuteButton.tsx
+++ src/pages/RequestView/RemakeRequestButton/CannotReExecuteButton.tsx
@@ -1,20 +1,26 @@
 import { Button, Tooltip } from '@material-ui/core'
+import { useTheme } from '@mui/material/styles'
 
 interface CannotReExecuteButtonProps {
   message: string
 }
 
 const CannotReExecuteButton = ({ message }: CannotReExecuteButtonProps) => {
+  const theme = useTheme()
   return (
     <Tooltip title={message} data-testid="cannot-execute-button">
       <Button
         component="div"
         disabled
         variant="contained"
-        color="secondary"
-        style={{ float: 'right', pointerEvents: 'auto' }}
+        style={{
+          float: 'right',
+          pointerEvents: 'auto',
+          // theme style override not applying for some reason
+          color: theme.palette.action.disabled,
+        }}
       >
-        {'Remake Request'}
+        Remake Request
       </Button>
     </Tooltip>
   )
