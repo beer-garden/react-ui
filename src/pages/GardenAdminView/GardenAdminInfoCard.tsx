230b3f4ebc2cc524e9c6a1c72a7667595ca4858f
west270 <westl270@outlook.com>
2023-05-10 17:45:00 +0000

    fixed spacing for garden admin pages

--- src/pages/GardenAdminView/GardenAdminInfoCard.tsx
+++ src/pages/GardenAdminView/GardenAdminInfoCard.tsx
@@ -1,4 +1,4 @@
-import { Box, List, ListItem, Typography } from '@mui/material'
+import { Grid, List, ListItem, Typography } from '@mui/material'
 import { LabeledData } from 'components/LabeledData'
 import { Garden } from 'types/backend-types'
 
@@ -12,19 +12,23 @@ const GardenAdminInfoCard = ({ garden }: GardenInfoCardProps) => {
       <Typography variant="h3" color="inherit">
         Garden Info
       </Typography>
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, 180px)',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: 1,
-        }}
+      <Grid
+        container
+        columns={3}
+        rowSpacing={1}
+        columnSpacing={4}
+        pl={4}
       >
-        <LabeledData label="Name" data={garden.name} />
-        <LabeledData label="Status" data={garden.status} alert />
-        <LabeledData label="Connection Type" data={garden.connection_type} />
-        <Box sx={{ gridColumnStart: '1' }}>
+        <Grid item >
+          <LabeledData label="Name" data={garden.name} />
+        </Grid>
+        <Grid item >
+          <LabeledData label="Status" data={garden.status} alert />
+        </Grid>
+        <Grid item >
+          <LabeledData label="Connection Type" data={garden.connection_type} />
+        </Grid>
+        <Grid item xs={3} >
           <Typography sx={{ my: 2 }} fontWeight={'bold'} variant="overline">
             Known Namespaces:
           </Typography>
@@ -42,8 +46,8 @@ const GardenAdminInfoCard = ({ garden }: GardenInfoCardProps) => {
               </ListItem>
             ))}
           </List>
-        </Box>
-      </Box>
+        </Grid>
+      </Grid>
     </>
   )
 }
