be9275729704c2a2d89d51b315fc9ac3ddc4d494
west270 <61320354+west270@users.noreply.github.com>
2023-07-11 22:36:21 -0400

    #424 - Fixed checkbox bug (#427)

--- src/pages/CommandView/ParameterElements/ParamCheckbox.tsx
+++ src/pages/CommandView/ParameterElements/ParamCheckbox.tsx
@@ -1,4 +1,5 @@
 import { FormCheckbox } from 'components/FormComponents'
+import { useFormContext } from 'react-hook-form'
 import { Parameter } from 'types/backend-types'
 
 interface ParamCheckboxProps {
@@ -7,13 +8,16 @@ interface ParamCheckboxProps {
 }
 
 const ParamCheckbox = ({ parameter, registerKey }: ParamCheckboxProps) => {
+
+  const { watch } = useFormContext()
+  const value = watch(registerKey)
   
   return (
     <FormCheckbox
       registerKey={registerKey}
       label={!parameter.multi ? parameter.display_name : ''}
       helperText={!parameter.multi ? parameter.description : ''}
-      registerOptions={{required: parameter.optional ? false : `${parameter.display_name} is required`}}
+      registerOptions={{required: parameter.optional || value === false ? false : `${parameter.display_name} is required`}}
     />
   )
 }
